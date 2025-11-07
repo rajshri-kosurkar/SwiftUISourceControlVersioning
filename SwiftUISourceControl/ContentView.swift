af4ec8b4f09b990ea38ea528dac8ae2023e9be1c
rajshri-kosurkar <kosurkarrajshri9@gmail.com>
2025-11-07 14:22:01 +0000

    Initial Commit

---
+++ SwiftUISourceControl/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUISourceControl
+//
+//  Created by Rajshri Kosurkar on 07/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUISourceControl/SwiftUISourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISourceControlApp.swift
+//  SwiftUISourceControl
+//
+//  Created by Rajshri Kosurkar on 07/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
