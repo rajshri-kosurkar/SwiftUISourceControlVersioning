2079dd746bc46b0e1b7ee657b9bf2d1810daa319
rajshri-kosurkar <kosurkarrajshri9@gmail.com>
2025-12-01 22:20:16 +0000

    Changes text to HomeScreen

--- SwiftUISourceControl/ContentView.swift
+++ SwiftUISourceControl/ContentView.swift
@@ -5,15 +5,34 @@
 //  Created by Rajshri Kosurkar on 07/11/25.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ RELEASE:
+ [Release] Description of release
+
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+
+ MUNDANE TASKS:
+ [Clean] Description of changes
+ 
+ */
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
+            Image(systemName: "heart.fill")
+                .font(.largeTitle)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("First project on GitHub")
         }
         .padding()
     }
---
+++ SwiftUISourceControl/HomeView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeView.swift
+//  SwiftUISourceControl
+//
+//  Created by Rajshri Kosurkar on 01/12/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+    }
+}
+
+#Preview {
+    HomeView()
+}
