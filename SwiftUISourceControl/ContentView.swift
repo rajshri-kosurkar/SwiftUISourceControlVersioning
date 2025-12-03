d59c1eafe601b2c6e9149a0ff7babd703757694a
rajshri-kosurkar <kosurkarrajshri9@gmail.com>
2025-12-03 18:52:05 +0000

    documentation

--- SwiftUISourceControl/ContentView.swift
+++ SwiftUISourceControl/ContentView.swift
@@ -6,6 +6,18 @@
 //
 
 /*
+ 
+Clone = Copying the repo locally
+Commit = Save ("checkpoint") changes on our current branch
+Stage = Prepare changes for a commit
+Stash = Save changes for later
+Push = Send local commits to remote repo
+Pull = Fetch remote commits to local repo
+Merge = Joining two different branch
+Rebase = Moving one branch on top of another branch
+Cherry Picking = Duplicating (copying) one commit from one branch to another
+
+ 
  COMMIT MESSAGES
  
  NEW FEATURE:
