8f740dff2911d0d9649bf30b264aaa91e1e6d04b
rajshri-kosurkar <kosurkarrajshri9@gmail.com>
2025-12-04 11:03:42 +0000

    different merge pull request options
    * [Clean] update documentation
    
    * worked on different merge options

--- SwiftUISourceControl/ContentView.swift
+++ SwiftUISourceControl/ContentView.swift
@@ -17,8 +17,9 @@ Pull = Fetch remote commits to local repo
 Merge = Joining two different branch
 Rebase = Moving one branch on top of another branch
 Cherry Picking = Duplicating (copying) one commit from one branch to another
- Pull Request (PR) = Request to merge branch
-
+Pull Request (PR) = Request to merge branch
+PR Merge = Merge all commits
+PR Squash and Merge = Squash all commits into ONE and then merge ONE commit
 ===============================================
  COMMIT MESSAGES
  
